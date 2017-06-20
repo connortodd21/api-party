5258b8d28ea8bde372489301cb4fe4fc7d782441
Connor Todd <todd37@purdue.edu>
2017-06-20 11:53:16 -0400

    Add submit handler and push to new history location. Additional styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route, NavLink } from 'react-router-dom'
 import './App.css';
+
+import Github from './Github'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h3>Ain't no party like an</h3>
-          <h1>API Party</h1>
+          <div className="App-headings">
+            <h3>Ain't no party like an</h3>
+            <h1>API Party</h1>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to='/github'>Github API</NavLink>
+            </li>
+          </ul>
         </div>
+        <Switch>
+          <Route path='/github' component={Github} />
+          <Route render={() => <p>To get started, click one of the links above</p>} />
+        </Switch>
       </div>
     );
   }
 }
-
 export default App;
